dda495fda60f9be7be67db8b4c2374e2d2976175
xiaofuyesnew <xiaofuyesnew@qq.com>
2023-11-22 10:49:55 +0800

    feat: update

--- src/utils.js
+++ src/utils.js
@@ -18,4 +18,16 @@ export const mkcertdir = `${homedir}/.koa-mkcert`
 export const mkcertfile = `${mkcertdir}/mkcert${platform === 'win32' ? '.exe' : ''}`
 
 console.log(process.arch)
-console.log()
+console.log(platform)
+
+if (platform === 'win32') {
+  console.log(`mkcert-v1.4.4-windows-${process.arch.includes('arm') ? 'arm64' : 'amd64'}.exe`)
+} else if (platform === 'darwin') {
+  console.log(`mkcert-v1.4.4-darwin-${process.arch.includes('arm') ? 'arm64' : 'amd64'}`)
+} else {
+  console.log(`mkcert-v1.4.4-linux-${process.arch.includes('arm') ? process.arch : 'amd64'}`)
+}
+
+//'aix''darwin''freebsd''linux''openbsd''sunos''win32'
+
+//'arm'、 'arm64'、 'ia32'、 'mips'、 'mipsel'、 'ppc'、 'ppc64'、 's390'、 's390x'、 'x32' 和 'x64'。
